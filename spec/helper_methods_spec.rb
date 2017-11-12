725749611e6eb31a89747ce98e363d6ac4c08fed
BLACKBAUD\Matt.Dilts <matt.dilts@blackbaud.com>
2017-11-12 15:23:51 -0500

    Update RSpec assertions

--- spec/helper_methods_spec.rb
+++ spec/helper_methods_spec.rb
@@ -8,13 +8,13 @@ describe "Testing helpers for email_helper's read_email method" do
                      :to => "bar@foo.com",
                      :subject => "Foo Bar"}
       transformed_options = test_collection.options_hash_to_array(option_hash)
-      transformed_options.is_a?(Array).should == true
+      expect(transformed_options.is_a?(Array)).to eq(true)
       option_hash.each_with_index do | keyvalue, index |
           key = keyvalue[0]
           value = keyvalue[1]
-         transformed_options[index*2].downcase.to_sym.should == key
-         transformed_options[index*2].should == key.to_s.upcase
-         transformed_options[index*2 + 1].should == value
+          expect(transformed_options[index*2].downcase.to_sym).to eq(key)
+          expect(transformed_options[index*2]).to eq(key.to_s.upcase)
+          expect(transformed_options[index*2 + 1]).to eq(value)
       end
   end
 
